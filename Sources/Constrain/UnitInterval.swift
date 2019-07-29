f1cb9f8ada42e5e80e0660a1c384dc5bfb8e3b42
Alex Young <alex.buzov@icloud.com>
2019-07-29 16:52:21 -0700

    codable

--- Sources/Constrain/UnitInterval.swift
+++ Sources/Constrain/UnitInterval.swift
@@ -1,10 +1,19 @@
 import Foundation
 
 @propertyWrapper
-public struct UnitInterval<Value: FloatingPoint> {
+public struct UnitInterval<Value> where Value: FloatingPoint & Codable {
     @Constrained(0...1) public var wrappedValue: Value = .zero
 
     public init(initialValue: Value) {
         self.wrappedValue = initialValue
+    }
+    
+    public init(from decoder: Decoder) throws {
+        self.wrappedValue = try decoder.singleValueContainer().decode(Value.self)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(wrappedValue)
     }
 }
